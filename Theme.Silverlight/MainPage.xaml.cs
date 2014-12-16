42ee7366f146f85450196eb1685b3b5f3045e9d2
Brian Lagunas <brianlagunas@users.noreply.github.com>
2014-12-16 13:43:47 -0700

    adding themes with sample

---
+++ Theme.Silverlight/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Theme.Silverlight
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            List<string> names = new List<string>();
+            names.Add("Anand");
+            names.Add("Arun");
+            names.Add("Arjun");
+            names.Add("Arul");
+            names.Add("Arula");
+            names.Add("Arule");
+            names.Add("Bala");
+            names.Add("Balaji");
+            names.Add("Barani");
+            names.Add("Buji");
+            names.Add("Brasil");
+            autoComplete.ItemsSource = names;
+        }
+
+        static int Clicks = 0;
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clicks += 1;
+            clickTextBlock.Text = "Number of Clicks: " + Clicks;
+        }
+    }
+}
---
+++ Theme.WPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Theme.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> names = new List<string>();
+            names.Add("Anand");
+            names.Add("Arun");
+            names.Add("Arjun");
+            names.Add("Arul");
+            names.Add("Arula");
+            names.Add("Arule");
+            names.Add("Bala");
+            names.Add("Balaji");
+            names.Add("Barani");
+            names.Add("Buji");
+            names.Add("Brasil");
+            autoComplete.ItemsSource = names;
+        }
+
+        static int Clicks = 0;
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clicks += 1;
+            clickTextBlock.Text = "Number of Clicks: " + Clicks;
+        }
+    }
+}
